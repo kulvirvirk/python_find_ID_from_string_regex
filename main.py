737f929e924805c264fa9b660cc4eb59f93f4518
kulvirvirk <kvirkdev@gmail.com>
2020-04-30 05:12:13 +0000

    added the regex logic to capture a id with alphabet and 6 digits. e.g. A123456

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ import re
 
 
 string_with_id = ('Hello, my id is A123456')
+
+# create regex for to capture: 
+# - alphabets A to Z and lowercase a to z = [[A-Za-z]]
+# - digits 0 to 9 for next 6 positons = \d\d\d\d\d\d
+# - combine both with raw string = r'[A-Za-z]\d\d\d\d\d\d'
+id_regex = re.findall(r'[A-Za-z]\d\d\d\d\d\d', string_with_id)
+print(id_regex)
